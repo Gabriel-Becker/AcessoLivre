836e02509bf64963009b9e13b0d2ea5e46ff5b5a
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2025-10-10 15:04:34 -0300

    feat: configuração inicial do projeto Spring Boot

---
+++ backend/src/test/java/com/example/acessolivre/AcessolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acessolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
