9bb5f4220a9a0c0a9bd88a6956e1c75757259a8e
Gabriel Becker <83714713+Gabriel-Becker@users.noreply.github.com>
2025-10-10 17:27:09 -0300

    feat: cria DTOs UsuarioRequestDTO e UsuarioResponseDTO

---
+++ backend/src/main/java/com/example/acessolivre/dto/UsuarioRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.acessolivre.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioRequestDTO {
+
+    private String nome;
+    private String email;
+    private String cpf;
+    private String role;
+    private String imagemPerfil;
+}
---
+++ backend/src/main/java/com/example/acessolivre/dto/UsuarioResponseDTO.java
@@ -0,0 +1,21 @@
+package com.example.acessolivre.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioResponseDTO {
+
+    private Integer idUsuario;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String role;
+    private LocalDateTime dataCadastro;
+    private String imagemPerfil;
+}
