b2313458a67afacb26d4fe78e28aa213315dce64
Sanha258 <alwayscleanwm@gmail.com>
2026-04-15 20:01:44 -0300

    chore: removido CRUD TipoAcessibilidade

--- backend/src/main/java/com/acessolivre/repository/TipoAcessibilidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acessolivre.repository;
-
-import com.acessolivre.model.TipoAcessibilidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoAcessibilidadeRepository extends JpaRepository<TipoAcessibilidade, Long> {
-}
