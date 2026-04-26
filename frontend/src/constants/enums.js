8ec00f174e80c88de3cfaa51dae65234858fd58f
Sanha258 <alwayscleamwm@gmail.com>
2026-04-26 11:19:53 -0300

    feat: criado tipo_acessibilidade enum front

--- frontend/src/constants/enums.js
+++ frontend/src/constants/enums.js
@@ -7,5 +7,18 @@ export const CATEGORIAS = [
   'Transporte' ,
   'Alimentacao',
   'Hospedagem' ,
-  'Servicos'
+  'Servicos',
+];
+
+export const TIPO_ACESSIBILIDADE = [
+  'Rampa de acesso' ,
+  'Banheiro adaptado', 
+  'Elevador acessível' ,
+  'Piso tátil',
+  'Sinalização em braille' ,
+  'Estacionamento acessível' ,
+  'Espaço amplo' ,
+  'Recursos audiovisuais' ,
+  'Atendimento especializado' ,
+  'Mobiliário adaptado',
 ];
