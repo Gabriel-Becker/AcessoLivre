beb7b6db847e908b6b45c413643e7c6b4dddc888
Sanha258 <alwayscleamwm@gmail.com>
2026-04-26 11:23:49 -0300

    chore: atualizado categoria enum back

--- backend/src/main/java/com/acessolivre/enums/Categoria.java
+++ backend/src/main/java/com/acessolivre/enums/Categoria.java
@@ -1,49 +1,13 @@
 package com.acessolivre.enums;
 
 public enum Categoria {
-    RESTAURANTE,
-    LANCHONETE,
-    CAFETERIA,
-    BAR,
-
-    HOSPITAL,
-    CLINICA,
-    FARMACIA,
-    LABORATORIO,
-
-    ESCOLA,
-    UNIVERSIDADE,
-    CRECHE,
-    BIBLIOTECA,
-
-    ORGAO_PUBLICO,
-    PREFEITURA,
-    FORUM,
-    DELEGACIA,
-    CORREIOS,
-    BANCO,
-
-    PRACA,
-    PARQUE,
-    PRAIA,
-    SHOPPING,
-    CINEMA,
-    TEATRO,
-    MUSEU,
-    GINASIO,
-
-    HOTEL,
-    POUSADA,
-
-    SUPERMERCADO,
-    MERCADO,
-    PADARIA,
-    LOJA,
-
-    IGREJA,
-    TEMPLO,
-
-    ESTACAO_ONIBUS,
-    AEROPORTO,
-    ESTACAO_TREM
+  COMERCIAL, 
+  PUBLICO, 
+  SAUDE ,
+  EDUCACAO ,
+  LAZER ,
+  TRANSPORTE ,
+  Alimentacao,
+  Hospedagem ,
+  Servicos
 }
