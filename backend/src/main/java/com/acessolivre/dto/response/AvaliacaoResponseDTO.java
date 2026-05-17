59e85582a6e7b4c8bf113901cac6bdf316d14693
Sanha258 <alwayscleamwm@gmail.com>
2026-05-17 18:50:55 -0300

    feat: adicionado avaliação response

---
+++ backend/src/main/java/com/acessolivre/dto/response/AvaliacaoResponseDTO.java
@@ -0,0 +1,22 @@
+mport lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvaliacaoResponseDTO {
+    private Long idAvaliacao;
+    private Integer notaAcessibilidadeVisual;
+    private Integer notaAcessibilidadeMotora;
+    private Integer notaAcessibilidadeAuditiva;
+    private Double notaGeral;
+    private String comentario; // opcional
+    private Boolean moderado;
+    private LocalDateTime dataAvaliacao;
+
+    private Long idUsuario;
+    private Long idLocal;
+}
