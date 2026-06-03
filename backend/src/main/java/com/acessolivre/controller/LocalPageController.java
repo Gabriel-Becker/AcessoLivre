f1ef86bc1450b1c7e0067cbf5649fb2e9cd9f23a
Sanha258 <alwayscleamwm@gmail.com>
2026-06-03 02:16:50 -0300

    feat: criado LocalPageController

---
+++ backend/src/main/java/com/acessolivre/controller/LocalPageController.java
@@ -0,0 +1,21 @@
+// LocalPageController.java
+package com.acessolivre.controller;
+
+import com.acessolivre.model.Local;
+import com.acessolivre.service.LocalService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/local")
+@RequiredArgsConstructor
+@Slf4j
+public class LocalPageController {
+
+    
+}
