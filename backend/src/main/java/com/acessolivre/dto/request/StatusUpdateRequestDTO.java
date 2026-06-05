fa301c467e8cd0b7375594d260325a1eb863373c
Sanha258 <alwayscleamwm@gmail.com>
2026-06-05 15:52:22 -0300

    feat: adicionado status e observações no request dto

---
+++ backend/src/main/java/com/acessolivre/dto/request/StatusUpdateRequestDTO.java
@@ -0,0 +1,20 @@
+package com.acessolivre.dto.request;
+
+import com.acessolivre.enums.StatusDenuncia;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatusUpdateRequestDTO {
+    
+    @NotNull(message = "Status é obrigatório")
+    private StatusDenuncia status;
+    
+    private String observacoes;
+}
