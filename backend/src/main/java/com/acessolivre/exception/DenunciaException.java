a9d3bc00b17b4c5945c31689e0ae48da62371fa0
Sanha258 <alwayscleamwm@gmail.com>
2026-06-06 22:31:12 -0300

    fix: corrigido nome dos construtores da classe DenunciaException

--- backend/src/main/java/com/acessolivre/exception/DenunciaException.java
+++ backend/src/main/java/com/acessolivre/exception/DenunciaException.java
@@ -1,11 +1,11 @@
 package com.acessolivre.exception;
 
 public class DenunciaException extends RuntimeException {
-    
+
     public DenunciaException(String message) {
         super(message);
     }
-    
+
     public DenunciaException(String message, Throwable cause) {
         super(message, cause);
     }
